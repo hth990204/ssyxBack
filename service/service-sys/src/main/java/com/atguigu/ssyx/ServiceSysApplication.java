f43925becc79acd54a91679fae0cbf75371ea86a
hth990204 <875177543@qq.com>
2023-06-09 23:34:49 +0800

    开通区域列表

---
+++ service/service-sys/src/main/java/com/atguigu/ssyx/ServiceSysApplication.java
@@ -0,0 +1,15 @@
+package com.atguigu.ssyx;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//权限管理模块启动类
+@SpringBootApplication
+public class ServiceSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceSysApplication.class, args);
+    }
+
+}
