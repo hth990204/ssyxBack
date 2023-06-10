06df3e6ffaf7b8b07b70c40eaf3a955c5e29e9aa
hth990204 <875177543@qq.com>
2023-06-10 18:40:23 +0800

    平台属性分组

--- service/service-product/src/main/java/com/atguigu/ssyx/product/service/impl/AttrGroupServiceImpl.java
+++ service/service-product/src/main/java/com/atguigu/ssyx/product/service/impl/AttrGroupServiceImpl.java
@@ -3,8 +3,16 @@ package com.atguigu.ssyx.product.service.impl;
 import com.atguigu.ssyx.model.product.AttrGroup;
 import com.atguigu.ssyx.product.mapper.AttrGroupMapper;
 import com.atguigu.ssyx.product.service.AttrGroupService;
+import com.atguigu.ssyx.vo.product.AttrGroupQueryVo;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +25,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class AttrGroupServiceImpl extends ServiceImpl<AttrGroupMapper, AttrGroup> implements AttrGroupService {
 
+    @Override
+    public IPage<AttrGroup> selectPageAttrGroup(Page<AttrGroup> pageParam, AttrGroupQueryVo attrGroupQueryVo) {
+        String name = attrGroupQueryVo.getName();
+        LambdaQueryWrapper<AttrGroup> wrapper = new LambdaQueryWrapper<>();
+        if (!StringUtils.isEmpty(name)) {
+            wrapper.like(AttrGroup::getName, name);
+        }
+        IPage<AttrGroup> attrGroupPage = baseMapper.selectPage(pageParam, wrapper);
+        return attrGroupPage;
+    }
+
+    @Override
+    public List<AttrGroup> findAllList() {
+        QueryWrapper<AttrGroup> wrapper = new QueryWrapper<>();
+        wrapper.orderByDesc("id");
+        List<AttrGroup> attrGroups = baseMapper.selectList(wrapper);
+        return attrGroups;
+    }
 }
