f1aa57e2693172894773a3083e779cb163801edb
hth990204 <875177543@qq.com>
2023-06-10 19:16:17 +0800

    平台属性分组设置属性列表

--- service/service-product/src/main/java/com/atguigu/ssyx/product/controller/AttrController.java
+++ service/service-product/src/main/java/com/atguigu/ssyx/product/controller/AttrController.java
@@ -1,9 +1,15 @@
 package com.atguigu.ssyx.product.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.atguigu.ssyx.common.result.Result;
+import com.atguigu.ssyx.model.product.Attr;
+import com.atguigu.ssyx.product.service.AttrService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
@@ -14,8 +20,57 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2023-06-10
  */
 @RestController
-@RequestMapping("/product/attr")
+@RequestMapping("/admin/product/attr")
+@CrossOrigin
+@Api(tags = "平台属性管理")
 public class AttrController {
+    @Resource
+    private AttrService attrService;
+
+    //      url: `${api_name}/${groupId}`,
+    //      method: 'get'
+    @ApiOperation("根据平台属性分组id查询")
+    @GetMapping("/{groupId}")
+    public Result list(@PathVariable Long groupId) {
+        List<Attr> list = attrService.getAttrListByGroupId(groupId);
+        return Result.ok(list);
+    }
+
+    @ApiOperation(value = "获取")
+    @GetMapping("get/{id}")
+    public Result get(@PathVariable Long id) {
+        Attr attr = attrService.getById(id);
+        return Result.ok(attr);
+    }
+
+    @ApiOperation(value = "新增")
+    @PostMapping("save")
+    public Result save(@RequestBody Attr attr) {
+        attrService.save(attr);
+        return Result.ok(null);
+    }
+
+    @ApiOperation(value = "修改")
+    @PutMapping("update")
+    public Result updateById(@RequestBody Attr attr) {
+        attrService.updateById(attr);
+        return Result.ok(null);
+    }
+
+    @ApiOperation(value = "删除")
+    @DeleteMapping("remove/{id}")
+    public Result remove(@PathVariable Long id) {
+        attrService.removeById(id);
+        return Result.ok(null);
+    }
+
+    @ApiOperation(value = "根据id列表删除")
+    @DeleteMapping("batchRemove")
+    public Result batchRemove(@RequestBody List<Long> idList) {
+        attrService.removeByIds(idList);
+        return Result.ok(null);
+    }
+
 
 }
 
